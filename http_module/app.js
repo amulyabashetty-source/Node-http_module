229b5f9bbccba457dfe22038f269801d0cbd349b
Amulya <amulyabashetty@gmail.com>
2026-03-20 17:19:43 +0530

    added cors fix

--- http_module/app.js
+++ http_module/app.js
@@ -231,7 +231,7 @@ const cors = require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:["http://localhost:5173"]}))
+app.use(cors());
 
 const data = [
     {
